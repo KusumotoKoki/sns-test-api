15f756270352fd4b84a405bc0596461243388481
KusumotoKoki <kusumotokoki0609@gmail.com>
2024-01-16 13:07:13 +0900

    簡単なsignup, loginを実装

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-// expressモジュールをインポートします
 const express = require('express');
-// expressアプリケーションを作成します
 const app = express();
+const cors = require('cors');
 
-// サーバーのポート番号を設定します
-const PORT = 5000;
+// corsミドルウェアを使用します
+app.use(cors());
 
 // express.jsonミドルウェアを使用します
 app.use(express.json());
@@ -12,10 +11,11 @@ app.use(express.json());
 const authRoute = require("./routers/auth");
 app.use("/api/auth", authRoute);
 
+// サーバーのポート番号を設定します
+const PORT = 5000;
 
 // 指定したポートでサーバーを起動します
 app.listen(PORT, () => {
   // サーバーが起動したことをコンソールにログ出力します
   console.log(`Server is running on port ${PORT}`);
 });
-
